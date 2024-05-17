dbce432aaeb13c85ca94d9f4b173f9ca5e4324c3
virenpinto1234 <v.pinto@lancaster.ac.uk>
2024-05-17 14:04:41 +0100

    first commit

--- read.java
+++ read.java
@@ -1,17 +1,96 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+
 
 public class read {
   public static void main(String[] args) {
     try {
-      File myObj = new File("/home/pintov/h-drive/Social-Network/social-network1.txt" );
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+      //File socNet = new File("/home/pintov/h-drive/Social-Network/social-network1.txt" );
+      File socNet = new File("C:\\Users\\Viren Pinto\\OneDrive\\Desktop\\Social-Network\\social-network1.txt" );
+      Scanner Read = new Scanner(socNet);
+      int nodecount = 0;
+      int totalEdgeCount = 0;
+      int followers = 0;
+      String name = " ";
+      ArrayList<String> names = new ArrayList<>();
+      ArrayList<Integer> arrayMedian = new ArrayList<>();
+      HashMap<String, Integer> followerCount = new HashMap<>();
+      
+
+      // loop checks if there is a next line
+      while (Read.hasNextLine()) 
+      {
+        nodecount = nodecount+1;
+        String line = Read.nextLine();
+        
+        Scanner lineScan = new Scanner(line);
+        int edgecount = 0;
+        
+        name = lineScan.next();
+        while(lineScan.hasNext())
+        {
+          String following = lineScan.next();
+          int currentFollowers = followerCount.getOrDefault(following, 0);
+          followerCount.put(following, currentFollowers + 1);
+          edgecount = edgecount + 1;
+        }
+    
+        lineScan.close();
+        
+        if(edgecount > followers)
+        {
+          followers = edgecount;
+          names.clear();
+          names.add(name);
+        }
+        else if(edgecount == followers)
+        {
+          names.add(name);
+        }
+  
+        totalEdgeCount = totalEdgeCount + edgecount;
+
       }
-      myReader.close();
+
+      for (Integer count : followerCount.values()) 
+      {
+        arrayMedian.add(count);
+       
+      }
+
+      float median = 0;
+      int size = arrayMedian.size();
+
+      //odd/even number Check 
+      if(size % 2 == 1) median = arrayMedian.get(size/2);
+      
+      else median = (arrayMedian.get(size/2 - 1) + arrayMedian.get(size/2)) / 2;
+
+      Read.close();
+
+
+      Collections.sort(names);
+      Collections.sort(arrayMedian);
+      float answer = (float) totalEdgeCount/(nodecount*(nodecount-1));
+
+      System.out.println("\nTotal number of nodes: " + nodecount);
+      System.out.println("\nTotal number of edges: " + totalEdgeCount);
+      System.out.println("\nTask 1: " + answer);
+      System.out.println("\nTask 3: " + names);
+      System.out.println("\nTask 5: " + median);
+      //System.out.println("\nTask 6: " + median);
+
+      // for (int i = 0; i<arrayMedian.size(); i++) {
+      // int curr = arrayMedian.get(i);
+
+//       System.out.println(curr);
+// }
+
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
